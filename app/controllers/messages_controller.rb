741895eaf4c8b1fe878cabcaf8c7791552e112fd
mt-aya <mmht31uv.u@gmail.com>
2020-11-24 19:52:24 +0900

    messagesコントローラーミス修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -11,7 +11,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path(@room)
     else
-      @message = @room.message.includes(:user)
+      @message = @room.messages.includes(:user)
       render :index
     end
   end
